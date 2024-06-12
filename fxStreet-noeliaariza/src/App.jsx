d8737d18f7edeff65a17ad72464b66bcc8e711bf
noeliaariza <noelia.ariza@gmail.com>
2024-06-12 15:19:19 +0200

    build(init commit): init first project structure

---
+++ fxStreet-noeliaariza/src/App.jsx
@@ -0,0 +1,14 @@
+//import { useState } from "react";
+import "./App.scss";
+
+function App() {
+  //const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <p>fxStreet!</p>
+    </>
+  );
+}
+
+export default App;
